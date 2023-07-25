19b8784427e2e2fe3782a64b0c5bef9d7aabdba2
star8in <sergeevem2007@gmail.com>
2023-07-25 21:52:40 +0400

    feat: add Header static template, style

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -2,5 +2,22 @@ import React, { FC } from 'react';
 import './Header.css';
 
 export const Header: FC = () => {
-  return <header></header>;
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="header__wrapper">
+          <div>
+            <a href="/" className="header__logo">
+              Пользователи гитхаба
+            </a>
+            <span className="header__nav">defunct</span>
+          </div>
+          <div>
+            <input type="text" name="" id="" className="header__input" placeholder="Поиск пользователя" />
+            <button className="header__btn">Найти</button>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
